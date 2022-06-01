82f9303e37c4ebadae7c0ea846809f42cf38bacf
Gabriel <chentevilo2001@gmail.com>
2022-05-31 23:03:32 -0600

    Portafolio

---
+++ src/Portafolio.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './Estilos/Portafolio.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <span>Portafolio</span>
+    </div>
+  )
+}
+
+export default App
